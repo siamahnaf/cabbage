0d4e48ebc0dbc738942cb6284f470ff43bec8057
Siam Ahnaf <siamahnaf198@gmail.com>
2022-02-16 00:55:24 +0600

    version@2.0.0 (Final Release)'

--- src/Utilis/Seo.js
+++ src/Utilis/Seo.js
@@ -3,15 +3,15 @@ import Head from 'next/head';
 const Seo = ({ title }) => {
     return (
         <Head>
-            <meta property="og:url" content="https://siamahnaf.com" />
+            <meta property="og:url" content="https://www.cabbageweb.tech/" />
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content="Cabbage is a fearless programming platform. Here you can find web based tutorial and full web development courses:) Programming is the best in demand job in the world. Web programming is also the another field to create life more easier. And make money by working less than a hour more and more. Freelancing is one of them:) We are here for learn you all the type of programming you want learn. Stay with us and make the day more happier:)" />
             <title>{title}</title>
             <meta name="description" content="Cabbage is a fearless programming platform. Here you can find web based tutorial and full web development courses:) Programming is the best in demand job in the world. Web programming is also the another field to create life more easier. And make money by working less than a hour more and more. Freelancing is one of them:) We are here for learn you all the type of programming you want learn. Stay with us and make the day more happier:)" />
             <link rel="shortcut icon" href="/favicon.ico" />
-            <meta name="keywords" content="Siam, Siam Ahnaf" />
-            <meta name="author" content="Siam Ahnaf" />
+            <meta name="keywords" content="Cabbage, cabage, cabage, cabbege, kabbage, cababage, kababage, programming with cabbage, program with cabbage, program by cabbage, programming by cabbage, kabbage, cabbage, cabage, cabb base, cabbase, kabbase, kabase, cab, cabb, kabb, kab base, kab bage, cabbage coding, cabbage coding, motivation, career, business, job, coding, freelancing, website design, learn to code, full stack, front end, developer, programmer, javascript, python, coding, css, html, web design, data science, mcahine learning" />
+            <meta name="author" content="Cabbage" />
         </Head>
     );
 };
